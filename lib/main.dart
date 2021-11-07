1fa015af5a9c87e4e398c097e93ad1cb7cb21ad2
Mizumoto Ryohei <mrcrius1355@gmail.com>
2021-11-07 19:39:27 +0900

    オンラインフレンド一覧画面と、ユーザー詳細画面を作成

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,49 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: OnlineFriendPage(),
+      home: const OnlineFriendPage(),
     );
   }
 }
 
 class OnlineFriendPage extends StatelessWidget {
+  const OnlineFriendPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('オンラインフレンド一覧'),
-    ));
+    return Scaffold(
+      body: const Center(
+        child: Text('オンラインフレンド一覧'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // pushした際に、フレンド詳細画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return const UserShowPage();
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class UserShowPage extends StatelessWidget {
+  const UserShowPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            // "pop"で前の画面に戻る
+            Navigator.of(context).pop();
+          },
+          child: const Text('ユーザー詳細（クリックで戻る）'),
+        ),
+      ),
+    );
   }
 }
