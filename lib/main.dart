97c87a81a60aedd1801d2a4a156381efe247c0ad
Mizumoto Ryohei <mrcrius1355@gmail.com>
2021-11-13 14:45:00 +0900

    create login form

--- lib/main.dart
+++ lib/main.dart
@@ -166,12 +166,77 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        // child: Text(
-        child: Text('Login'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Log in'),
+        ),
+        body: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                  child: ConstrainedBox(
+                      constraints:
+                          BoxConstraints(minHeight: constraints.maxHeight),
+                      child: Padding(
+                          padding: const EdgeInsets.all(32.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  TextField(
+                                    controller: null,
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                      ),
+                                      labelText: 'Username/Email:',
+                                    ),
+                                  ),
+                                  const SizedBox(height: 24.0),
+                                  TextField(
+                                    controller: null,
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                      ),
+                                      labelText: 'password',
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Container(
+                                width: double.infinity,
+                                height: 54.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: TextButton(
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.blue),
+                                    foregroundColor:
+                                        MaterialStateProperty.all(Colors.white),
+                                  ),
+                                  onPressed: () {
+                                    // submit!
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context) {
+                                      return const OnlineFriendsPage();
+                                    }));
+                                  },
+                                  child: const Text('login'),
+                                ),
+                              ),
+                            ],
+                          ))));
+            },
+          ),
+        ));
   }
 }
 
